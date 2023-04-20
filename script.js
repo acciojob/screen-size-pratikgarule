1b1de2fd4d0b57c7448fb8c71d556d9c533e74f4
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-04-20 11:02:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+window.addEventListener('resize', function() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let sizeInfo = document.querySelector('#sizeInfo');
+    sizeInfo.innerHTML = '<h1>Width: ' + width + 'px' + ' Height: ' + height + 'px</h1>';
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+	 let width = window.innerWidth;
+    let height = window.innerHeight;
+    let sizeInfo = document.querySelector('#sizeInfo');
+    sizeInfo.innerHTML = '<h1>Width: ' + width + 'px' + ' Height: ' + height + 'px</h1>';
+});
